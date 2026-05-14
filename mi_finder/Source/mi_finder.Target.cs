b17d9d40f0a0d7a5bc6461ef59f7c8b159fcb06c
TERABYTE0130 <engineerillust@gmail.com>
2026-05-15 00:37:35 +0900

    5.7のビルドを通した

--- mi_finder/Source/mi_finder.Target.cs
+++ mi_finder/Source/mi_finder.Target.cs
@@ -8,8 +8,8 @@ public class mi_finderTarget : TargetRules
 	public mi_finderTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("mi_finder");
 	}
 }
